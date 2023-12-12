1dcdf014ecef041faaaeb13490d3ee2120122af0
gaurav4883 <gaurav.deol1600@gmail.com>
2023-12-12 22:43:15 +0545

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,6 +37,7 @@ function Contact() {
         setButtonText("Send");
         let result = await response.json();
         setFormDetails(formInitialDetails);
+
         if (result.code === 200) {
             setStatus({ succes: true, message: 'Message sent successfully' });
         } else {
@@ -56,7 +57,7 @@ function Contact() {
                         <h2>Stay Connected</h2>
                         <form onSubmit={handleSubmit}>
                             <Row>
-                                <Col sm={6} className="px-11">
+                                <Col sm={6} className="px-111">
                                     <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                                 </Col>
                                 <Col sm={6} className="px-11">
@@ -68,9 +69,9 @@ function Contact() {
                                 <Col sm={6} className="px-11">
                                     <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                                 </Col>
-                                <Col size={12} className="px-1">
+                                <Col size={12} className="px-11">
                                     <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                                    <button type="submit"><span>{buttonText}</span></button>
+                                    <button type="submit"  ><span>{buttonText}</span></button>
                                 </Col>
                                 {
                                     status.message &&
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,17 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 
 
 function Footer() {
+    const marginTopStyle = { marginTop: '30px' };
+
     return (
         <footer className='footer' >
             <Container>
-                <Row className='align-item-center' >
+                <Row className='align-item-center'  >
                     <Col sm={6} >
-                        <img src={logo} alt="logo" />
+                        <img src={logo} alt="logo" style={marginTopStyle} />
                     </Col>
                     <Col sm={6} className="text-center text-sm-end " >
-                        <div className="social-icon">
+                        <div className="social-icon" style={marginTopStyle}>
                             <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/gaurav-deol-9648aa212/"><img src={navIcon1} alt="" /></a>
                             <a target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/gaurav.dewal.56/"><img src={navIcon2} alt="" /></a>
                             <a target="_blank" rel="noreferrer noopener" href="https://www.instagram.com/__mr.gaurav/"><img src={navIcon3} alt="" /></a>
@@ -27,5 +29,6 @@ function Footer() {
         </footer>
     )
 }
+
 
 export default Footer
